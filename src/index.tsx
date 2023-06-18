bc9bda858a49613f159b36607235e6e4afe5422b
taniafate <micheartist@yandex.ru>
2023-06-18 22:56:20 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/configureStore';
 
@@ -10,9 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store = {store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
